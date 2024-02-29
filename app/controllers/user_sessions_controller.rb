bab494da6421d9f4aff781124bfabae7d21167e5
Yuya-Ikeda-1994 <yuya_eg_sp@ymail.ne.jp>
2024-02-29 15:24:59 +0900

    フラッシュメッセージ作成

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -6,16 +6,16 @@ class UserSessionsController < ApplicationController
     @user = login(user_params[:email], user_params[:password])
 
     if @user
-      redirect_back_or_to(root_path)
+      redirect_back_or_to(root_path, notice: t('user_sessions.create.success'))
     else
-      flash.now[:alert] = "サインインに失敗しました。再度お試しください"
+      flash.now[:alert] = t('user_sessions.create.fall')
       render :new
     end
   end
 
   def destroy
     logout
-    redirect_to root_path, notice: 'サインアウトしました'
+    redirect_to root_path, notice: t('user_sessions.destroy.success')
   end
 
   private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,25 @@
 class UsersController < ApplicationController
   def new
-    # ログイン済みならユーザ作成画面にはアクセスできない
     if logged_in?
       redirect_to root_url
     end
-
     @user = User.new
   end
 
-  def create(user)
-    @user = User.new(user.permit(:first_name, :last_name, :email, :password, :password_confirmation, :bio, :avatar, :gender))
+  def create
+    @user = User.new(user_params) 
 
     if @user.save
-      redirect_to new_user_session_path, notice: 'ユーザー登録に成功しました。ログインしてください。'
+      redirect_to new_user_session_path, notice: t('users.create.success') 
     else
+      flash.now[:alert] = t('users.create.fall') 
       render :new
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :bio, :avatar, :gender)
   end
 end
