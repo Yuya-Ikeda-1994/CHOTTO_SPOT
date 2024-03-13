07c8da4845609f0b6478904b5de7bec942a73847
Yuya-Ikeda-1994 <yuya_eg_sp@ymail.ne.jp>
2024-03-13 10:20:58 +0900

    デプロイエラー修正_6

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,24 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-10.times do                            
-    User.create!(
-      last_name: Faker::Name.unique.last_name,
-      first_name: Faker::Name.unique.first_name,
-      email: Faker::Internet.unique.email,
-      password: "12345678",
-      password_confirmation: "12345678"
-    )
-  end
-  
-  8.times do |index|                    
-    Spot.create!(
-      user: User.offset(rand(User.count)).first,
-      spot_name: "タイトル#{index}",
-      address: "住所#{index}",
-      comment: "コメント#{index}"
-    )
-  end
+
 
   Tag.create!([
   { content: '屋外' },
